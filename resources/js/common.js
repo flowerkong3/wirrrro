a49b0e5e17c14141a6fb121eb207f1adeaae42c6
min <90238684+flowerkong3@users.noreply.github.com>
2022-10-10 13:22:01 +0900

    서브페이지 제작

---
+++ resources/js/common.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+
+	const btnTop = document.querySelector('.btn--gotoTop');
+	const hamburger = document.querySelector('.hamburger');
+	const nav = document.querySelector('.nav');
+	const gnb = document.querySelector('.gnb');
+	const sns = document.querySelector('.nav__right');
+
+
+
+	// Mobile hamburger
+	hamburger.addEventListener('click',function(){
+		hamburger.classList.toggle('on');
+		nav.classList.toggle('on');
+		gnb.classList.toggle('on');
+		sns.classList.toggle('on');
+	})
+
+
+	// * Scroll | Show Hide | gotoTop 
+	$('.btn--gotoTop').hide()
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 200){
+			$('.btn--gotoTop').show()
+		}else {
+			$('.btn--gotoTop').hide();
+		}
+	})
+
+
+
+
+
+	// * Scroll | Scroll Top
+	btnTop.addEventListener('click',function(){
+		event.preventDefault();
+		window.scrollTo(0,0);
+	})
+
+
+}
